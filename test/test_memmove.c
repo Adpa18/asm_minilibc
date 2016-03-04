7217ea2b94bef05e6f9e2a8fa44faeadea58c0a8
Adrien WERY <adrien.wery@epitech.eu>
2016-03-04 11:20:48 +0100

    memmove

--- test/test_memmove.c
+++ test/test_memmove.c
@@ -2,17 +2,15 @@
 #include <stdio.h>
 #include <string.h>
 
-int	main(int argc, char *argv[])
+int	main()
 {
-  char tmp[] = "tsmjkfrjsdlkfjpoer";
-  char tmp2[] = "abcdefghijklmnopqrstiov";
-  char	*str;
+    char tmp2[] = "abcdefghijklmnopqrstiov";
+    char tmp[] = "adpa";
+    // char tmp2[] = "ababcdehijklmnopqrstiov";
 
   memmove(NULL, NULL, 0);
-  str = strdup(tmp);
   printf("str => %s | ", tmp2);
   memmove(&tmp2[2], &tmp2[0], 5);
   printf("str -> %s\n", tmp2);
-  free(str);
   return 0;
 }
